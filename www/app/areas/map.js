8ea0b85bf38095b168df41c1042296f3908067ec
Benedicte Raae <raae@bgraphic.no>
2016-08-03 20:23:00 +0200

    Added area layers to map.

--- www/app/areas/map.js
+++ www/app/areas/map.js
@@ -1,8 +1,9 @@
 angular
     .module('Varsom')
-    .controller('AreasMapCtrl', function ($scope, $stateParams, $ionicLoading, Area, Utility, areas) {
+    .controller('AreasMapCtrl', function ($scope, $stateParams, $ionicLoading, $http, Area, Utility, areas) {
 
         var vm = this;
+
         vm.areas = areas;
 
         vm.center = {
@@ -18,6 +19,14 @@ angular
             url: "http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png"
         }
 
+        $http.get("/geojson/" + $stateParams.areaType + ".geojson").success(function (data, status) {
+
+            vm.geojson = {
+                data: data
+            };
+
+            console.log(vm.geojson);
+        });
 
         $scope.$on("areas.refreshed", function (event, args) {
             console.log("AreasMapCtrl: areas.refreshed ", args);
