7eea12a5ac719baa17d040bdb21e0c20cc3c5c2d
Benedicte Raae <raae@bgraphic.no>
2016-08-03 13:34:40 +0200

    Refactored tranformation of areas

--- www/app/areas/list.js
+++ www/app/areas/list.js
@@ -2,47 +2,13 @@ angular
     .module('Varsom')
     .controller('AreasListCtrl', function ($scope, $stateParams, $ionicLoading, Area, Utility, areas) {
 
-        function updateForecasts(areas) {
-            angular.forEach(areas, function (area) {
-                if (Area.isRegion(area)) {
-                    var avalancheForecast = Utility.chooseLanguage(Area.getForecast(area));
-                    avalancheForecast = avalancheForecast ? avalancheForecast : [{DangerLevel: 0}, {DangerLevel: 0}, {DangerLevel: 0}];
-                    area.forecast = avalancheForecast;
-                } else {
-                    var floodForecast = Utility.chooseLanguage(Area.getForecast(area, "flood"));
-                    var landslideForecast = Utility.chooseLanguage(Area.getForecast(area, "landslide"));
-
-                    floodForecast = floodForecast ? floodForecast : [{ActivityLevel: 0}, {ActivityLevel: 0}, {ActivityLevel: 0}];
-                    landslideForecast = landslideForecast ? landslideForecast : [{ActivityLevel: 0}, {ActivityLevel: 0}, {ActivityLevel: 0}];
-
-                    area.forecast = [];
-
-                    for (var i = 0; i < 3; i++) {
-                        if (floodForecast[i].ActivityLevel > landslideForecast[i].ActivityLevel) {
-                            area.forecast.push(floodForecast[i]);
-                        } else {
-                            area.forecast.push(landslideForecast[i]);
-                        }
-                    }
-
-                }
-            });
-        }
-
         var vm = this;
-
         vm.areas = areas;
 
         $scope.$on("areas.refreshed", function (event, args) {
             console.log("AreasListCtrl: areas.refreshed ", event, args);
             Area.getAreas($stateParams.areaType, $stateParams.parentId).then(function (areas) {
                 vm.areas = areas;
-                updateForecasts(vm.areas);
             });
-        });
-
-        $scope.$watch(vm.areas, function () {
-            console.log("AreasListCtrl: changes to vm.areas");
-            updateForecasts(vm.areas);
         });
     });
--- www/app/services/Area.js
+++ www/app/services/Area.js
@@ -2,6 +2,58 @@ angular
     .module('Varsom')
     .provider('Area', function () {
 
+        function isCounty(area) {
+            if (area.hasOwnProperty('countyId') && !area.hasOwnProperty('municipalityId')) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        function isMunicipality(area) {
+            if (area.hasOwnProperty('municipalityId')) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        function isRegion(area) {
+            if (area.hasOwnProperty('regionId')) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        function getId(area) {
+            if (area.hasOwnProperty('municipalityId')) {
+                return area.municipalityId;
+            }
+
+            if (area.hasOwnProperty('countyId')) {
+                return area.countyId;
+            }
+
+            if (area.hasOwnProperty('regionId')) {
+                return area.regionId;
+            }
+        }
+
+        function getForecast(area, forecastType) {
+            var forecastString = null;
+
+            if (area.hasOwnProperty("regionId")) {
+                forecastString = area["avalancheWarningForecast"];
+            } else {
+                forecastString = area[forecastType + "WarningForecast"];
+            }
+
+            if (forecastString) {
+                return JSON.parse(forecastString);
+            }
+        }
+
         function setAreas(areas, areaType, parentId) {
             if (parentId) {
                 storage[areaType + parentId] = areas;
@@ -16,6 +68,39 @@ angular
             } else {
                 return storage[areaType];
             }
+        }
+
+        function getHighestForecast(forecast1, forecast2) {
+            var highestForecast = [];
+
+            forecast1 = forecast1 ? forecast1 : [{ActivityLevel: 0}, {ActivityLevel: 0}, {ActivityLevel: 0}];
+            forecast2 = forecast2 ? forecast2 : [{ActivityLevel: 0}, {ActivityLevel: 0}, {ActivityLevel: 0}];
+
+            for (var i = 0; i < 3; i++) {
+                if (forecast1[i].ActivityLevel > forecast2[i].ActivityLevel) {
+                    highestForecast.push(forecast1[i]);
+                } else {
+                    highestForecast.push(forecast2[i]);
+                }
+            }
+
+            return highestForecast;
+        }
+
+        function transformAreas(areas) {
+            angular.forEach(areas, function (area) {
+                console.log("transforming");
+                if (isRegion(area)) {
+                    area.forecast = getForecast(area);
+                } else {
+                    var floodForecastNo = getForecast(area, "flood").no;
+                    var landslideForecastNo = getForecast(area, "landslide").no;
+
+                    area.forecast = {
+                        no: getHighestForecast(floodForecastNo, landslideForecastNo)
+                    }
+                }
+            });
         }
 
         function loadAreas(areaType, parentId) {
@@ -36,7 +121,10 @@ angular
             console.log("AreaProvider: Loading areas ", areaType, query, options);
             return appstax.find(areaType, query, options).then(function (areas) {
                 console.log("AreaProvider: Loaded areas ", areas);
+                transformAreas(areas);
+                console.log("AreaProvider: Transformed areas ", areas);
                 setAreas(areas, areaType, parentId);
+                return getAreas(areas, areaType, parentId);
             });
         }
 
@@ -62,8 +150,6 @@ angular
                             return getAreas(areaType, parentId);
                         });
                     } else {
-                        var defer;
-                        defer = $q.defer();
                         return $q.when(getAreas(areaType, parentId));
                     }
                 },
@@ -75,53 +161,10 @@ angular
                         return getAreas(areaType, parentId);
                     });
                 },
-                isCounty: function (area) {
-                    if (area.hasOwnProperty('countyId') && !area.hasOwnProperty('municipalityId')) {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                },
-                isMunicipality: function (area) {
-                    if (area.hasOwnProperty('municipalityId')) {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                },
-                isRegion: function (area) {
-                    if (area.hasOwnProperty('regionId')) {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                },
-                getId: function (area) {
-                    if (area.hasOwnProperty('municipalityId')) {
-                        return area.municipalityId;
-                    }
-
-                    if (area.hasOwnProperty('countyId')) {
-                        return area.countyId;
-                    }
-
-                    if (area.hasOwnProperty('regionId')) {
-                        return area.regionId;
-                    }
-                },
-                getForecast: function (area, forecastType) {
-                    var forecastString = null;
-
-                    if (area.hasOwnProperty("regionId")) {
-                        forecastString = area["avalancheWarningForecast"];
-                    } else {
-                        forecastString = area[forecastType + "WarningForecast"];
-                    }
-
-                    if (forecastString) {
-                        return JSON.parse(forecastString);
-                    }
-                }
+                isCounty: isCounty,
+                isMunicipality: isMunicipality,
+                isRegion: isRegion,
+                getId: getId
             };
         }
     });
