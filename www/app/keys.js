4eb417b4b4938f96b4d4dd0d61c0c07d30e9364b
Benedicte Raae <raae@bgraphic.no>
2016-09-06 19:00:28 +0200

    Update keys and config files

---
+++ www/app/keys.js
@@ -0,0 +1,8 @@
+angular
+  .module('Varsom')
+  .constant('AppKeys', {
+    "ionic": {
+        "appId": "f11eace1",
+        "appKey": "2db186347847c5d1e3dacea7bd2d1cc5465980bf2cfff335"
+    }
+  });
