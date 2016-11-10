2be9a3829c1f81eed3ea91ccdb276fafa9a6bb01
Benedicte Raae <raae@bgraphic.no>
2016-11-10 15:48:33 +0100

    Data label above warning badge

--- src/partials/warning-badge.ts
+++ src/partials/warning-badge.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'nve-warning-badge',
-  template: '<ion-badge item-left color="level{{level}}" class="nve-warning-badge">{{level}}</ion-badge>',
+  template: '<ion-badge color="level{{level}}">{{level}}</ion-badge>',
 })
 export class WarningBadge {
   @Input() level: number;
