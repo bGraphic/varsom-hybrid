b936e3657e17d9bc984c05de451803ebcecf4c43
Benedicte Raae <raae@bgraphic.no>
2016-12-28 14:42:00 +0100

    Wait for platform is ready and watch position until there is one #111

--- src/services/settings.ts
+++ src/services/settings.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { Platform } from 'ionic-angular';
 import { Geolocation } from 'ionic-native';
 
 @Injectable()
@@ -8,13 +9,22 @@ export class SettingsService {
   private _currentForecastType = new BehaviorSubject<string>('highest');
   private _currentPosition = new BehaviorSubject({ latLng: L.latLng(64.871, 16.949), zoom: 4 });
 
-  constructor () {
-    Geolocation.getCurrentPosition().then((resp) => {
-      this._currentPosition.next({ latLng: L.latLng(resp.coords.latitude, resp.coords.longitude), zoom: 6 });
-      console.log('Settings: Updating user location', resp);
-    }, (error) => {
-      console.log('Settings: Error getting location', error);
+  constructor (public platform: Platform) {
+
+    this.platform.ready().then(() => {
+      let subscription = Geolocation.watchPosition()
+        .filter((p:any) => {
+          return p.code === undefined; //Filter Out Errors
+        })
+        .map((p:any) => {
+          return { latLng: L.latLng(p.coords.latitude, p.coords.longitude), zoom: 6 }
+        })
+        .subscribe((p:{ latLng: L.LatLng, zoom: number }) => {
+          this._currentPosition.next(p);
+          subscription.unsubscribe();
+        });
     });
+
   }
 
   get currentForecastTypeObs():Observable<string> {
