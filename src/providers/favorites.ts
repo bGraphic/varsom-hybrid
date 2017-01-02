9babf06cd3d126c278cc7424690a300d5773b489
Benedicte Raae <raae@bgraphic.no>
2017-01-02 09:42:41 +0100

    Refactored favorites provider #39

--- src/providers/favorites.ts
+++ src/providers/favorites.ts
@@ -1,56 +1,96 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import 'rxjs/add/operator/map';
-import { Observable, BehaviorSubject } from "rxjs";
+import {Observable, BehaviorSubject, ReplaySubject} from "rxjs";
 
 @Injectable()
 export class Favorites {
+
   private readonly FAVORITES_KEY = 'favorites';
-  private _favorites$ = new BehaviorSubject<string[]>([]);
+  private _favoriteAreaIds$ = new BehaviorSubject<string[]>([]);
+  private _changeEvents$ = new ReplaySubject<{type:string, areaIds:string[]}>();
 
   constructor(private _storage: Storage) {
-    this._fetchFavorites();
+
+    this.favoriteAreaIds$.subscribe(favorites => {
+      console.log("Favorites: Changed", favorites);
+    });
+
+    this._fetchInitialFavorites$()
+      .concat(this._changeEvents$.publishReplay().refCount())
+      .subscribe(event => {
+        console.log("Favoriets: Event", event.type, event.areaIds);
+
+        switch(event.type) {
+          case 'initial':
+            this._favoriteAreaIds$.next(event.areaIds);
+            break;
+          case 'add':
+            this._addFavoriteArea(event.areaIds);
+            break;
+          case 'remove':
+            this._removeFavoriteArea(event.areaIds);
+            break;
+        }
+
+      });
   }
 
-  get favorites$():Observable<string[]> {
-    return this._favorites$.asObservable();
+  get favoriteAreaIds$():BehaviorSubject<string[]> {
+    return this._favoriteAreaIds$;
+  }
+
+  removeFavoriteArea(areaId: string) {
+    this._changeEvents$.next({type:'remove', areaIds: [areaId]});
   }
 
   addFavoriteArea(areaId: string) {
-    let favorites = this._favorites$.getValue();
-    let areaAlreadyFavorite = favorites.filter(favorite => favorite === areaId).length > 0;
-    if(!areaAlreadyFavorite) {
-      favorites.push(areaId);
-      this._favorites$.next(favorites);
-    };
+    this._changeEvents$.next({type:'add', areaIds: [areaId]});
   }
 
-  addFavoriteAreas(areaIds: string[]) {
-    for(let areaId of areaIds) {
-      this.addFavoriteArea(areaId);
+  private _addFavoriteArea(areaIds: string[]) {
+    let favorites = this._favoriteAreaIds$.getValue();
+    let changed = false;
+    for (let areaId of areaIds){
+      let index = favorites.indexOf(areaId);
+      if( index === -1) {
+        favorites.push(areaId);
+        changed = true;
+      }
+    }
+
+    if(changed) {
+      this._favoriteAreaIds$.next(favorites);
+      this._saveFavorites(favorites);
     }
   }
 
-  private _fetchFavorites() {
-    this._storage.get(this.FAVORITES_KEY)
-      .then(
-        favorites => {
-          console.log("Favorites: Fetched favorites", favorites);
-          if(favorites) {
-            this.addFavoriteAreas(favorites);
-          }
-          this._favorites$
-            .distinctUntilChanged()
-            .subscribe(
-              favorites => {
-                this._saveFavorites(favorites);
-              }
-            );
-        },
-        error => {
-          console.error("Favorites: error fetching favorites", error)
-        }
-      );
+  private _removeFavoriteArea(areaIds: string[]) {
+    let favorites = this._favoriteAreaIds$.getValue();
+    let changed = false;
+    for (let areaId of areaIds){
+      let index = favorites.indexOf(areaId);
+      if( index > -1) {
+        favorites = favorites.slice(0, index).concat(favorites.slice(index+1, favorites.length));
+        changed = true;
+      }
+    }
+
+    if(changed) {
+      this._favoriteAreaIds$.next(favorites);
+      this._saveFavorites(favorites);
+    }
+  }
+
+  private _fetchInitialFavorites$():Observable<{type:string, areaIds:string[]}> {
+    return Observable
+      .fromPromise(this._storage.get(this.FAVORITES_KEY))
+      .filter(favorites => {
+        return !!favorites;
+      })
+      .map(favorites => {
+        return { type: 'initial', areaIds: favorites}
+      });
   }
 
   private _saveFavorites(favorites: string[]) {
