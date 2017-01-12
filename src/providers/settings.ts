3cdfd356b4e36df760369c2a668d8a7b3a99b35b
Benedicte Raae <raae@bgraphic.no>
2017-01-12 13:17:53 +0100

    Save selected flood/landslide active segment with storage #113

--- src/providers/settings.ts
+++ src/providers/settings.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import {BehaviorSubject, Observable, Subject} from 'rxjs';
 import { Platform } from 'ionic-angular';
 import { Geolocation } from 'ionic-native';
+import { Storage } from '@ionic/storage';
 import { TranslateService } from 'ng2-translate';
 
 @Injectable()
 export class SettingService {
 
-  private _activeFloodLandslideSegment$ = new BehaviorSubject<string>('highest');
+  private readonly ACTIVE_FLOOD_LANDSLIDE_SEGMENT_KEY = 'ACTIVE_FLOOD_LANDSLIDE_SEGMENT_KEY';
+
+  private _activeFloodLandslideSegment$ = new BehaviorSubject<string>(null);
   private _currentPosition$ = new BehaviorSubject({ latLng: L.latLng(64.871, 16.949), zoom: 4 });
   private _sections: {titleKey: string, icon: string, active:boolean, component: any }[];
 
   constructor (
     public platform: Platform,
-    private _translateService: TranslateService
+    private _translateService: TranslateService,
+    private _storage: Storage
   ) {
 
     this.platform.ready().then(() => {
@@ -29,6 +33,17 @@ export class SettingService {
           subscription.unsubscribe();
         });
     });
+
+    this._activeFloodLandslideSegment$
+      .skip(1)
+      .subscribe(value => {
+        this._saveSetting(this.ACTIVE_FLOOD_LANDSLIDE_SEGMENT_KEY, value);
+      });
+
+    this._fetchSavedValue(this.ACTIVE_FLOOD_LANDSLIDE_SEGMENT_KEY)
+      .subscribe(value => {
+        this._activeFloodLandslideSegment$.next(value)
+      });
 
   }
 
@@ -60,6 +75,32 @@ export class SettingService {
         section.active = false;
       }
     }
+  }
+
+  private _fetchSavedValue(key):Observable<string> {
+    return Observable
+      .fromPromise(this._storage.get(key))
+      .map(value => {
+        if(!value) {
+          if(this.ACTIVE_FLOOD_LANDSLIDE_SEGMENT_KEY === key) {
+            return 'highest';
+          }
+        } else {
+          return value;
+        }
+      })
+  }
+
+  private _saveSetting(key, value) {
+    this._storage.set(key, value)
+      .then(
+        () => {
+          console.log("SettingsService: Saved", key, value);
+        },
+        error => {
+          console.error("SettingsService: Error storing", key, value);
+        }
+      );
   }
 
 }
