a68a2db2b8ebc2fb922c60d2741783a763a5e8bb
Benedicte Raae <raae@bgraphic.no>
2017-05-10 11:19:59 +0200

    Remove token from favorites reduces, and fix problem with use of both pushToken and token in push.

--- src/store/reducers/favorites.reducer.ts
+++ src/store/reducers/favorites.reducer.ts
@@ -3,12 +3,10 @@ import * as localStorage from './../actions/local-storage.actions';
 
 export interface State {
   areaIds: string[];
-  pushToken: string;
 };
 
 const initialState: State = {
   areaIds: [],
-  pushToken: null
 }
 
 export function reducer(state = initialState, action: favorites.Actions | localStorage.Actions): State {
@@ -29,8 +27,6 @@ export function reducer(state = initialState, action: favorites.Actions | localS
       return Object.assign({}, state, {
         // Will override areaIds on load sucess
         areaIds: [...action.payload.favoritesAreaIds],
-        // Will not override in case new push token is set
-        pushToken: state.pushToken ? state.pushToken : action.payload.pushToken
       });
     default:
       return state;
@@ -38,4 +34,3 @@ export function reducer(state = initialState, action: favorites.Actions | localS
 }
 
 export const getAreaIds = (state: State) => state.areaIds;
-export const getPushToken = (state: State) => state.pushToken;
--- src/store/reducers/push.reducer.ts
+++ src/store/reducers/push.reducer.ts
@@ -1,25 +1,34 @@
 import { IPushMessage } from '@ionic/cloud-angular';
+
 import * as push from './../actions/push.actions';
+import * as localStorage from './../actions/local-storage.actions';
 
 export interface State {
   token: string | null;
+  savedToken: string | null;
   messages: IPushMessage[];
 }
 
 const initialState: State = {
   token: null,
+  savedToken: null,
   messages: []
 };
 
-export function reducer(state = initialState, action: push.Actions): State {
+export function reducer(state = initialState, action: push.Actions | localStorage.Actions): State {
   switch (action.type) {
     case push.REGISTER_SUCESS:
       return Object.assign({}, state, {
-        pushToken: action.payload.token
+        token: action.payload.token
       });
     case push.NEW_MESSAGE:
       return Object.assign({}, state, {
         messages: [...state.messages, action.payload]
+      });
+    case localStorage.LOAD_SUCESS:
+      return Object.assign({}, state, {
+        // Will not override in case new push token has been set
+        savedToken: action.payload.pushToken
       });
     default:
       return state;
