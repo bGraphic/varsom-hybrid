723fdacfbeca0bb2f281db12e0978f513b25fc8b
Benedicte Raae <raae@bgraphic.no>
2017-12-01 12:13:23 +0100

    Fetch geojson on platform ready

--- src/store/effects/geojson.effects.ts
+++ src/store/effects/geojson.effects.ts
@@ -1,32 +1,47 @@
 import { Injectable } from "@angular/core";
-import { Action } from "@ngrx/store";
+import { Action, Store } from "@ngrx/store";
 import { Actions, toPayload, Effect } from "@ngrx/effects";
 import { Observable } from "rxjs/Observable";
 import * as regionsActions from "./../actions/regions.actions";
 import * as geojsonActions from "./../actions/geojson.actions";
 import { GeojsonService } from "../services/geojson.service";
 import { of } from "rxjs/observable/of";
+import { Platform } from "ionic-angular";
+import * as fromRoot from "./../../store/reducers";
 
 @Injectable()
 export class GeojsonEffects {
   constructor(
     private _actions$: Actions,
-    private _geojsonService: GeojsonService
-  ) {}
+    private _geojsonService: GeojsonService,
+    private _platform: Platform,
+    private _store: Store<fromRoot.State>
+  ) {
+    this._platform.ready().then(() => {
+      this._store.dispatch(new geojsonActions.FetchAllAction());
+    });
+  }
 
   @Effect()
   fetchRegions$: Observable<Action> = this._actions$
-    .ofType(regionsActions.FETCH)
-    .map(toPayload)
-    .map(payload => {
-      return new geojsonActions.FetchAction({
-        sectionType: payload.sectionType
-      });
-    });
+    .ofType(geojsonActions.FETCH_ALL)
+    // .startWith(new geojsonActions.FetchAllAction())
+    .mergeMap(() => {
+      return Observable.from([
+        new geojsonActions.FetchAction({
+          sectionType: "FloodLandslide"
+        }),
+        new geojsonActions.FetchAction({
+          sectionType: "Avalanche"
+        })
+      ]);
+    })
+    .do(test => console.log(test));
 
   @Effect()
   fetchGeojsonObjects$: Observable<Action> = this._actions$
     .ofType(geojsonActions.FETCH)
+    .do(test => console.log(test))
     .map(toPayload)
     // Group by so that switch map only happens on the same sectionType
     .groupBy(payload => payload.sectionType)
