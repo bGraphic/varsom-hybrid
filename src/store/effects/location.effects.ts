d46130bb516641ce9ad5ea61deb520225acba749
Benedicte Raae <raae@bgraphic.no>
2017-12-21 01:18:55 +0100

    Check for platform ready before watching geolocation.

--- src/store/effects/location.effects.ts
+++ src/store/effects/location.effects.ts
@@ -1,20 +1,30 @@
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Observable";
 import { of } from "rxjs/observable/of";
-import { Effect, Actions } from "@ngrx/effects";
-import { Action } from "@ngrx/store";
+import { Effect, Actions, toPayload } from "@ngrx/effects";
+import { Action, Store } from "@ngrx/store";
 import { Geolocation } from "@ionic-native/geolocation";
+import { Platform } from "ionic-angular";
 
+import * as fromRoot from "./../../store/reducers";
 import * as locationActions from "./../actions/location.actions";
 
 @Injectable()
 export class LocationEffects {
-  constructor(private _actions$: Actions, private _geolocation: Geolocation) {}
+  constructor(
+    private _actions$: Actions,
+    private _store: Store<fromRoot.State>,
+    private _geolocation: Geolocation,
+    private _platform: Platform
+  ) {
+    this._platform.ready().then(() => {
+      this._store.dispatch(new locationActions.WatchPosition());
+    });
+  }
 
   @Effect()
   watchPosition$: Observable<Action> = this._actions$
     .ofType(locationActions.WATCH_POSITION)
-    .startWith(new locationActions.WatchPosition())
     .switchMapTo(this._geolocation.watchPosition())
     .map(res => {
       return {
@@ -31,4 +41,17 @@ export class LocationEffects {
         })
       );
     });
+
+  @Effect({ dispatch: false })
+  sucess$: Observable<Action> = this._actions$
+    .ofType(locationActions.POSITION_UPDATED)
+    .do(payload => console.log("[Location] Position Updated"))
+    .mapTo(null);
+
+  @Effect({ dispatch: false })
+  error$: Observable<Action> = this._actions$
+    .ofType(locationActions.POSITION_ERROR)
+    .map(toPayload)
+    .do(error => console.warn("[Location] Position failed", error))
+    .mapTo(null);
 }
